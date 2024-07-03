9653aa978ece413dc403fb03d6672a408eb300da
Mahesh Zalte <93540281+zaltemahesh7@users.noreply.github.com>
2024-07-03 06:56:45 +0000

    Updated information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mahesh Zalte",
+  title: "Web Developer & Content Creator",
+  email: "maheshzalte2000@gmail.com",
+  gitHub: "zaltemahesh7",
+  instagram: "zalte.mahesh.7",
+  linkedIn: "maheshzalte2000",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "maheshzalte2000",
+  youTube: "@imahesh2686",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/8.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,21 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Passionate web developer with a Bachelor's in Computer Science (B.C.S), currently pursuing an MCA. Skilled in JavaScript, React JS, Node.js, MongoDB, HTML5, CSS3, TailwindCSS, Git, and GitHub.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web design | Development",
   "User experience",
   "Inclusive design",
   "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Responsive interfaces",
+  "JavaScript",
+  "ReactJs",
+  "NextJs",
 ];
 
 /**
@@ -45,7 +47,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
